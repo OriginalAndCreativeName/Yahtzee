ccf70b886c6710cc69cb4da66b72566f827863a5
Keshav Ganapathy <55774025+keshavganapathy@users.noreply.github.com>
2019-12-17 22:17:57 -0500

    Add files via upload

--- GamerStrategy.java
+++ GamerStrategy.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class GamerStrategy extends YahtzeeComputerStrategy {
 
 	private AbstractYahtzeeCombination[] availableCombinations;
+
 	@Override
 	public String playerName() {
 		// TODO Auto-generated method stub
@@ -9,14 +12,22 @@ public class GamerStrategy extends YahtzeeComputerStrategy {
 
 	@Override
 	public void reroll(int[] dice, int rollNumber, PlayerRecord record, boolean[] reroll) {
-		int[] diceval = {0,0,0,0,0};
+		int[] diceval = { 0, 0, 0, 0, 0 };
 		int ones = 0;
 		int sixes = 0;
 		int twos = 0;
 		int threes = 0;
 		int fours = 0;
 		int fives = 0;
-		int holder = 0;
+		int holder = 6;
+		int holder2 = 6;
+		int holder3 = 6;
+		int holder4 = 6;
+		int holder5 = 6;
+		int holder6 = 6;
+		int place1 = 0;
+		int place2 = 0;
+		int place3 = 0;
 		for (int i = 0; i < dice.length; i++) {
 			holder = dice[i];
 			if (holder == 1) {
@@ -33,109 +44,134 @@ public class GamerStrategy extends YahtzeeComputerStrategy {
 				sixes++;
 			}
 		}
-		holder = 0;
-		int holder2 = 0;
-		if((ones == 5) || (twos == 5) || (threes == 5) || (fours == 5) || (fives == 5) || (sixes == 5)) {
+		if ((ones == 5) || (twos == 5) || (threes == 5) || (fours == 5) || (fives == 5) || (sixes == 5)) {
 			setBool(reroll, false, false, false, false, false);
-		}else if (((ones == 1) && (twos == 1) && (threes == 1) && (fours == 1) && (fives == 1)) || ((twos == 1) && (threes == 1) && (fours == 1) && (fives == 1) && (sixes == 1))) {
+		} else if (((ones == 1) && (twos == 1) && (threes == 1) && (fours == 1) && (fives == 1))
+				|| ((twos == 1) && (threes == 1) && (fours == 1) && (fives == 1) && (sixes == 1))) {
 			setBool(reroll, false, false, false, false, false);
-		}else if((ones == 4) || (twos == 4) || (threes == 4) || (fours == 4) || (fives == 4) || (sixes == 4)) {
-			if(ones == 1) {
+		} else if ((ones == 4) || (twos == 4) || (threes == 4) || (fours == 4) || (fives == 4) || (sixes == 4)) {
+			if (ones == 1) {
 				holder = 1;
-				if(isIn(dice,1)) {
-					holder = getIndex(dice,1);
+				if (isIn(dice, 1)) {
+					holder = getIndex(dice, 1, 0);
 				}
-			}else if(twos == 1) {
+			} else if (twos == 1) {
 				holder = 2;
-				if(isIn(dice,2)) {
-					holder = getIndex(dice,2);
+				if (isIn(dice, 2)) {
+					holder = getIndex(dice, 2, 0);
 				}
-			}else if(threes == 1) {
+			} else if (threes == 1) {
 				holder = 3;
-				if(isIn(dice,3)) {
-					holder = getIndex(dice,3);
+				if (isIn(dice, 3)) {
+					holder = getIndex(dice, 3, 0);
 				}
-			}else if(fours ==  1) {
+			} else if (fours == 1) {
 				holder = 4;
-				if(isIn(dice,4)) {
-					holder = getIndex(dice,4);
+				if (isIn(dice, 4)) {
+					holder = getIndex(dice, 4, 0);
 				}
-			}else if(fives == 1) {
+			} else if (fives == 1) {
 				holder = 5;
-				if(isIn(dice,5)) {
-					holder = getIndex(dice,5);
+				if (isIn(dice, 5)) {
+					holder = getIndex(dice, 5, 0);
 				}
-			}else if(sixes == 1) {
+			} else if (sixes == 1) {
 				holder = 6;
-				if(isIn(dice,6)) {
-					holder = getIndex(dice,6);
+				if (isIn(dice, 6)) {
+					holder = getIndex(dice, 6, 0);
 				}
 			}
-			if(holder == 0) {
+			if (holder == 0) {
 				setBool(reroll, true, false, false, false, false);
-			}else if(holder == 1) {
+			} else if (holder == 1) {
 				setBool(reroll, false, true, false, false, false);
-			}else if (holder == 2) {
+			} else if (holder == 2) {
 				setBool(reroll, false, false, true, false, false);
 			} else if (holder == 3) {
 				setBool(reroll, false, false, false, true, false);
 			} else if (holder == 4) {
 				setBool(reroll, false, false, false, false, true);
 			}
-		}else if((ones == 3) || (twos == 3) || (threes == 3) || (fours == 3) || (fives == 3) || (sixes == 3)) {
-			holder = 6; 
-			holder2 = 6;
-			if(ones == 3) {
-				if((twos ==2)||(threes == 2)|| (fours == 2)||(fives == 2)||(sixes == 2)) {
-					setBool(reroll, false, false, false, false, false);
-				}
-			}else if(twos ==3) {
-				if((ones ==2)||(threes == 2)|| (fours == 2)||(fives == 2)||(sixes == 2)) {
-					setBool(reroll, false, false, false, false, false);
-				}
-			}else if(threes == 3) {
-				if((twos ==2)||(ones == 2)|| (fours == 2)||(fives == 2)||(sixes == 2)) {
-					setBool(reroll, false, false, false, false, false);
-				}
-			}else if(fours == 3) {
-				if((twos ==2)||(threes == 2)|| (ones == 2)||(fives == 2)||(sixes == 2)) {
-					setBool(reroll, false, false, false, false, false);
-				}
-			}else if(fives == 3) {
-				if((twos ==2)||(threes == 2)|| (fours == 2)||(ones == 2)||(sixes == 2)) {
-					setBool(reroll, false, false, false, false, false);
-				}
-			}else if(sixes == 3) {
-				if((twos ==2)||(threes == 2)|| (fours == 2)||(fives == 2)||(ones == 2)) {
+		} else if ((ones == 3) || (twos == 3) || (threes == 3) || (fours == 3) || (fives == 3) || (sixes == 3)) {
+			place1 = 0;
+			place2 = 0;
+			place3 = 0;
+			if (ones == 3) {
+				place1 = getIndex(dice,1,0);
+				place2 = getIndex(dice,1,place1);
+				place3 = getIndex(dice,1,place2);
+				if ((twos == 2) || (threes == 2) || (fours == 2) || (fives == 2) || (sixes == 2)) {
 					setBool(reroll, false, false, false, false, false);
 				}else {
-					
+					if(twos == 1) {
+						holder = getIndex(dice,2,0);
+					}else if(threes == 1) {
+						holder2 = getIndex(dice,3,0);
+					}else if(fours == 1) {
+						holder3 = getIndex(dice,4,0);
+					}else if(fives == 1) {
+						holder4 = getIndex(dice,5,0);
+					}else if(sixes == 1) {
+						holder5 = getIndex(dice,6,0);
+					}
+					int[] stuff = setMax(holder, holder2, holder3, holder4, holder5);
+					setBool(reroll, stuff[0] == 6, stuff[1] == 6, stuff[2] == 6, stuff[3] == 6, stuff[4] == 6);
+				}
+			} else if (twos == 3) {
+				if ((ones == 2) || (threes == 2) || (fours == 2) || (fives == 2) || (sixes == 2)) {
+					setBool(reroll, false, false, false, false, false);
+				}
+			} else if (threes == 3) {
+				if ((twos == 2) || (ones == 2) || (fours == 2) || (fives == 2) || (sixes == 2)) {
+					setBool(reroll, false, false, false, false, false);
+				}
+			} else if (fours == 3) {
+				if ((twos == 2) || (threes == 2) || (ones == 2) || (fives == 2) || (sixes == 2)) {
+					setBool(reroll, false, false, false, false, false);
+				}
+			} else if (fives == 3) {
+				if ((twos == 2) || (threes == 2) || (fours == 2) || (ones == 2) || (sixes == 2)) {
+					setBool(reroll, false, false, false, false, false);
+				}
+			} else if (sixes == 3) {
+				if ((twos == 2) || (threes == 2) || (fours == 2) || (fives == 2) || (ones == 2)) {
+					setBool(reroll, false, false, false, false, false);
+				} else {
+
 				}
 			}
 		}
-		
+
 		// TODO Auto-generated method stub
 
 	}
+
 	private boolean isIn(int[] dice, int desired) {
-		for(int i = 0; i < dice.length; i++) {
-			if(dice[i]== desired) {
+		for (int i = 0; i < dice.length; i++) {
+			if (dice[i] == desired) {
 				return true;
 			}
 		}
 		return false;
 	}
-	private int getIndex(int[] dice, int desired) {
+
+	private int getIndex(int[] dice, int desired, int start) {
 		int target = 0;
-		for(int i = 0; i < dice.length; i++) {
-			if(dice[i] == desired) {
+		for (int i = start; i < dice.length; i++) {
+			if (dice[i] == desired) {
 				target = i;
 				return target;
 			}
 		}
-		//unreachable
+		// unreachable
 		return -1;
 	}
+	private int[] setMax(int one2, int two2, int three2, int four2, int five2) {
+		int[] returnVal = {one2, two2, three2, four2, five2};
+		Arrays.sort(returnVal);
+		return returnVal;
+	}
+
 	private void setBool(boolean[] reroll, boolean a, boolean b, boolean c, boolean d, boolean e) {
 		reroll[0] = a;
 		reroll[1] = b;
